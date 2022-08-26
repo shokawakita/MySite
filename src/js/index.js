a975d8340cd41e3edaf589ebef43cb4d12724709
川北翔 <kawakitasho@kawakitashounoMacBook-Pro.local>
2022-08-27 00:17:10 +0900

    fixe

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import '../scss/main.scss';
 import '../scss/footer.scss';
 import '../scss/menu.scss';
 import '../scss/pageback.scss';
+
 import API from './API';
 import menu from './menu';
 import pageTop from './pageTop';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
     mode: 'development',
@@ -8,7 +8,7 @@ module.exports = {
     // JavaScriptのコンパイル先とファイル名を設定
     // この場合はdistフォルダにbundle.jsという名前でコンパイルする
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: outputPath,
         filename: 'bundle.js'
     },
     module: {
@@ -25,36 +25,20 @@ module.exports = {
                         }
                     }
                 ],
-                exclude: /node_modules/,
             },
             {
-                test: /\.(sa|sc|c)ss$/,
+                test: /\.scss$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false,
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
                 ]
             }
         ]
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            // SASSのコンパイル後のファイル名を設定
-            // コンパイル先はoutputに設定したフォルダ
-            // この場合はdistフォルダにstyle.cssという名前でコンパイルする
-            filename: 'style.css',
-        })
-    ],
-    // devServer: {
-    //     static: {
-    //         directory: path.resolve(__dirname,'dist'),
-    //     }
-    // },
+    devServer: {
+        static: {
+            directory: outputPath,
+        }
+    },
 };
